fd0b364a82c46157be11b773b367a342b9725499
dyee <dyee@air.org>
2018-10-25 16:10:24 -0400

    Initial commit 2

--- work.py
+++ work.py
@@ -1,1 +1,13 @@
 from xml.etree import ElementTree
+
+file_path = "R:\\project\\DSY\\XML Codebook\\M48NT2AT.xml"
+
+my_tree = ElementTree.parse(file_path)
+root = my_tree.getroot()
+
+for df in root.iter('DataField'):
+    print('-----------------')
+    for d in df.attrib:
+        print('{0}: {1} (type: {2})'.format(d, df.attrib[d], type(df.attrib[d])))
+
+        
